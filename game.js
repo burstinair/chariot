db5838192d80df9166f1b6082376d7566e12994e
Burst <burstinair@gmail.com>
2013-05-14 00:47:43 +0800

    bug fix: multi item use

--- game.js
+++ game.js
@@ -333,6 +333,7 @@ Game.prototype.run = function () {
                             });
                             break;
                     }
+                    break;
                 }
             }
         }
